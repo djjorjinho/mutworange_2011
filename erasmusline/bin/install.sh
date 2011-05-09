f3263b028c3a5b69fce6d10a01cb7b425dfcf085
Daniel Lopes <dl.lopes@gmail.com>
2011-05-09 17:29:56 +0100

    unix "fresh install & test" script test script tweaks

---
+++ erasmusline/bin/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#
+# install script for UNIX systems
+#
+
+# main dir
+dir=$(cd `dirname $0` && pwd)
+dir="${dir}/../"
+cd ${dir}
+
+# main scripts
+cd scripts
+./run_sql
+
+cd ..
+
+# p8 stats
+cd stats/scripts
+./run_sql
+./populateDB.php
+cd ../bin
+./restart_daemons
+
+cd ../test
+./run
+
+cd ..
