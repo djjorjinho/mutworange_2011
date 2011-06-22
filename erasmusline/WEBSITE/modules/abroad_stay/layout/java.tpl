050ab56f771ea3e78eedda37d9153b447483b3f3
StV <stv@user-pc.(none)>
2011-06-22 10:05:35 +0300

    changes

---
+++ erasmusline/WEBSITE/modules/abroad_stay/layout/java.tpl
@@ -0,0 +1,13 @@
+        <script src="core/js/jquery/jquery-1.5.js" type="text/javascript"></script>
+        <script src="core/js/jquery/jquery.validationEngine-en.js" type="text/javascript" charset="utf-8"> </script>
+        <script src="core/js/jquery/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
+        <script src="core/js/custom.js" type="text/javascript" charset="utf-8"> </script>
+        <script src="core/js/jquery/sorttable.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="core/css/Style.css" type="text/css"/>
+            <link rel="stylesheet" href="core/css/validationEngine.jquery.css" type="text/css"/>
+<script type="text/javascript">
+           jQuery(document).ready(function(){
+           // binds form submission and fields to the validation engine
+           jQuery("#form1").validationEngine();
+       });
+</script> 
--- erasmusline/WEBSITE/modules/acom_reg/acom_reg.db.php
+++ erasmusline/WEBSITE/modules/acom_reg/acom_reg.db.php
@@ -1,87 +1,88 @@
 <?php
 
 class acom_regDB {
-        public static function checkDB() {
+
+    public static function checkDB() {
         $db = PlonkWebsite::getDB();
-        $studentId=  PlonkSession::get('id');
+        $studentId = PlonkSession::get('id');
         $stInfo = $db->retrieve("SELECT * FROM forms 
-            where  studentId='".$db->escape($studentId)."'
+            where  studentId='" . $db->escape($studentId) . "'
             and erasmusLevelId=10");
 
         return $stInfo;
     }
-    
+
     public static function checkLevel() {
         $db = PlonkWebsite::getDB();
-        $studentId=  PlonkSession::get('id');
+        $studentId = PlonkSession::get('id');
         $stInfo = $db->retrieve("SELECT statusOfErasmus, action FROM erasmusstudent 
-            where  users_email='".$db->escape($studentId)."'");
+            where  users_email='" . $db->escape($studentId) . "'");
 
         return $stInfo;
     }
-    
+
     public static function getInst($stId) {
         $db = PlonkWebsite::getDB();
         $stInfo = $db->retrieve("SELECT instName,iBan,bic,instStreetNr,instPostalCode,instCity FROM institutions where  instEmail=(select hostInstitutionId from 
-            erasmusstudent where users_email='".$db->escape($stId)."')");
+            erasmusstudent where users_email='" . $db->escape($stId) . "')");
 
         return $stInfo;
     }
 
-    
     //Fills tpl withs Student Info
     public static function getStudentInfo($matrNum) {
         $db = PlonkWebsite::getDB();
         $stInfo = $db->retrieve("SELECT ers.homeInstitutionId,ers.hostInstitutionId,u.firstName, u.familyName, u.sex,u.tel,u.email,u.birthDate,u.birthPlace FROM users as u 
-                JOIN erasmusstudent as ers on u.email = ers.users_email where u.email ='".$db->escape($matrNum)."'");
+                JOIN erasmusstudent as ers on u.email = ers.users_email where u.email ='" . $db->escape($matrNum) . "'");
 
 
         return $stInfo;
     }
-
 
     //Fille tpl with Institution Info
     public static function getInstInfo($instId) {
         $db = PlonkWebsite::getDB();
-        $stInfo = $db->retrieve("SELECT instName FROM institutions where  instEmail='".$db->escape($instId)."'");
+        $stInfo = $db->retrieve("SELECT instName FROM institutions where  instEmail='" . $db->escape($instId) . "'");
 
         return $stInfo;
     }
-    
+
     //Gets Available Residences
-    public static function getResidences($stId){
+    public static function getResidences($stId) {
         $db = PlonkWebsite::getDB();
         $acom = $db->retrieve("
                 SELECT * FROM residence as r where r.available='1' and country=(
                 SELECT ins.instCountry from institutions as ins where ins.instEmail=(
-                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='".$db->escape($stId)."')
+                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='" . $db->escape($stId) . "')
                    ) ORDER BY r.price ASC ");
         return $acom;
     }
-    public static function getResidence($stId,$resId){
+
+    public static function getResidence($stId, $resId) {
         $db = PlonkWebsite::getDB();
         $acom = $db->retrieve("
                 SELECT * FROM residence as r where country=(
                 SELECT ins.instCountry from institutions as ins where ins.instEmail=(
-                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='".$db->escape($stId)."')
-                   ) And r.residenceId='".$db->escape($resId)."'");
+                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='" . $db->escape($stId) . "')
+                   ) And r.residenceId='" . $db->escape($resId) . "'");
         return $acom;
     }
-    public static function getResidenceAV($stId,$resId){
+
+    public static function getResidenceAV($stId, $resId) {
         $db = PlonkWebsite::getDB();
         $acom = $db->retrieve("
                 SELECT * FROM residence as r where country=(
                 SELECT ins.instCountry from institutions as ins where ins.instEmail=(
-                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='".$db->escape($stId)."')
-                   ) And r.residenceId='".$db->escape($resId)."' AND r.available=1");
+                SELECT ers.hostInstitutionId from erasmusstudent as ers where ers.users_email='" . $db->escape($stId) . "')
+                   ) And r.residenceId='" . $db->escape($resId) . "' AND r.available=1");
         return $acom;
     }
-    
-         public static function SubmitTranscript($form, $name,$_POST) {
+
+    public static function SubmitTranscript($form, $name, $_POST) {
 
         $db = PlonkWebsite::getDB();
         $homeInst = $db->retrieve("SELECT ers.hostInstitutionId FROM erasmusstudent as ers 
-            where ers.users_email ='".$db->escape($name)."' ");
+            where ers.users_email ='" . $db->escape($name) . "' ");
         $mail = new PHPMailer();
         $mail->IsSMTP();
         $mail->SMTPAuth = true;
@@ -102,18 +103,26 @@ class acom_regDB {
             return $mail->ErrorInfo;
         } else {
             unset($_POST['formAction'], $_POST['postForm']);
-        $formTable = json_encode($_POST);
-        $formId=  Functions::createRandomString();
-        $date = date("y-m-d");
-        $query2 = "INSERT INTO forms (formId,type,date,content,studentId,erasmusLevelId) VALUES ( '".$db->escape($formId)."', 'Accomodation Register','" . $db->escape($date) . "','" . $db->escape($formTable) . "','" . $db->escape($name) . "','10') ";
-        $db->execute($query2);
-        if (isset($_POST['startDate'])){
-        $query1 = "UPDATE residence SET 
+            $formTable = json_encode($_POST);
+            $formId = Functions::createRandomString();
+            $date = date("y-m-d");
+            $query2 = "INSERT INTO forms (formId,type,date,content,studentId,erasmusLevelId) VALUES ( '" . $db->escape($formId) . "', 'Accomodation Register','" . $db->escape($date) . "','" . $db->escape($formTable) . "','" . $db->escape($name) . "','10') ";
+            $db->execute($query2);
+            if (isset($_POST['startDate'])) {
+                $query1 = "UPDATE residence SET 
                 available = '0'
                 WHERE residenceId='" . $db->escape($_POST['res']) . "'
                 ";
-        $db->execute($query1);           
-        }
+                $db->execute($query1);
+                $query2 = "INSERT INTO leasing 
+                    (startDate,endDate,residentId,studentId) VALUES 
+                    ( '" . $db->escape($_POST['startDate']) . "',
+                      '" . $db->escape($_POST['endDate']) . "',
+                      '" . $db->escape($_POST['res']) . "',
+                      '" . $db->escape($name) . "') ";
+
+                $db->execute($query2);
+            }
             return '1';
         }
     }
