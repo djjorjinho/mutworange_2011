c98b7fc14486cfa3b00d56f94ee9d19d9ce91ad6
Stephane Polet <stephan.polet@gmail.com>
2011-06-23 00:54:27 +0300

    fixing
    Signed-off-by: Stephane Polet <stephan.polet@gmail.com>

--- erasmusline/WEBSITE/modules/acom_reg/acom_reg.php
+++ erasmusline/WEBSITE/modules/acom_reg/acom_reg.php
@@ -50,6 +50,7 @@ class acom_regController extends PlonkController {
         $java = new PlonkTemplate(PATH_MODULES . '/' . MODULE . '/layout/java.tpl');
         $this->mainTpl->assign('pageJava', $java->getContent(true));
         $this->pageTpl->assign('errorString', $this->error);
+        $this->mainTpl->assign('breadcrumb','');
         $this->error = '';
 
         $checkFilled = acom_regDB::checkDB();
@@ -148,6 +149,26 @@ class acom_regController extends PlonkController {
         if ($return == '1') {
             $this->position = '3';
             $this->error = '<div class="SuccessPHP"><p>Your Application Was Success</p></div>';
+            $erasmusLevel = ExtendDB::getErasmusLevelId('Accomodation Registration Form');
+            
+            $valueEvent = array(
+                'reader' => 'Student',
+                'timestamp' => date("Y-m-d"),
+                'motivation' => '',
+                'studentId' => PlonkSession::get('id'),
+                'action' => 2,
+                'erasmusLevelId' => $erasmusLevel['levelId'],
+                'eventDescrip' => 'Filled in Accomodation Registration Form but you didn\'t want a room.',
+                'readIt' => 0
+            );
+
+            $er = array(
+                'statusOfErasmus' => 'Accomodation Registration Form',
+                'action' => 2
+            );
+
+            ExtendDB::insertValues('studentsEvents', $valueEvent);
+            ExtendDB::insertValues('erasmusStudent', $er, 'users_email = "'.PlonkSession::get('id').'"');
         } else {
             $this->position = '2no';
             $this->error = '<div class="errorPHP"><p>' . $return . '</p><p>Try Sending it again</p></div>';
